d799d20e4bd2a31314777915b9763073558ff129
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-06-13 12:58:57 +0530

    introduced installer login

--- server.js
+++ server.js
@@ -22,9 +22,11 @@ app.get('/clientauth', (request, response) => {
 app.post('/clientauth', (request, response) => {
   const { fssaiCode } = request.body;
   const { password } = request.body;
+  
   if (fssaiCode && password) {
-    response.send('Client login working.');
-    response.end();
+    request.session.loggedin = true;
+    request.session.username = fssaiCode;
+    response.redirect('/clienthome');
   } else {
     response.send('Please enter Username and Password!');
     response.end();
@@ -39,7 +41,35 @@ app.get('/clienthome', (request, response) => {
   }
   response.end();
 });
-// ----------------------------------------client login------------------------------------
+// ----------------------------------------Installation login------------------------------------
+app.get('/instauth', (request, response) => {
+  response.sendFile(path.join(`${__dirname}/installationlogin.html`));
+});
+
+app.post('/instauth', (request, response) => {
+//  const { fssaiCode } = request.body;
+  const { password } = request.body;
+  if (password === '21232f297a57a5a743894a0e4a801fc3') {
+    request.session.loggedin = true;
+    request.session.username = 'admin';
+    response.redirect('/insthome');
+  } else {
+    response.send('Incorrect Username and/or Password!');
+  }
+  response.end();
+});
+
+app.get('/insthome', (request, response) => {
+  if (request.session.loggedin) {
+    response.send(`Welcome back, ${request.session.username}!`);
+    request.session.destroy();
+  } else {
+    response.redirect('/instauth');
+  }
+  response.end();
+});
+
+// -----------------------------------------SERVER-------------------------
 app.listen(process.env.PORT || 4000, () => {
   // eslint-disable-next-line no-console
   console.log(`Your node js server is running at ${process.env.PORT}`);
