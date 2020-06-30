4975530709eb0e3c236d39bcf680c8616867ca26
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-06-30 19:31:12 +0530

    setup amazon rds

--- dbconfig.js
+++ dbconfig.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 
 exports.con = mysql.createConnection({
-  host: 'un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  user: 'cbdz3wr545xoy86u',
-  password: 'p5v5nvitxtfhn8ja',
+  host: 'connetdb.cbndeljvlxvj.ap-south-1.rds.amazonaws.com',
+  user: 'connettech',
+  password: 'Shankhanil1998',
   port: 3306,
-  database: 'ze7tokabnbf40r2r',
+  database: 'connetdb',
 });
